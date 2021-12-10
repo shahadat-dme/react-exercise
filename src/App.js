90ffd73a5f4fc21a33319fb8db63ae405b4ae953
Shahadat <shahadat2cp17@gmail.com>
2021-12-10 20:49:43 +0600

    form2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
-import MyForm from './form/MyForm';
+import MyForm2 from './form/MyForm2';
+// import MyForm1 from './form/MyForm1';
+// import MyForm from './form/MyForm';
 
 
 function App() {
   return (
     <div className="App">
       Hello, I am Coming
-      <MyForm/>
+      {/* <MyForm/> */}
+      {/* <MyForm1/> */}
+      <MyForm2/>
     </div>
   );
 }
---
+++ src/form/MyForm1.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function MyForm1() {
+  const [name, setName] = useState("");
+
+  return (
+    <form>
+      <label>Enter your name:
+        <input
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+    </form>
+  )
+}
+
+export default MyForm1;
