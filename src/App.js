5c45e276977eb05759001b917a34d5b332fe3417
Shahadat <shahadat2cp17@gmail.com>
2021-12-10 20:41:25 +0600

    myform

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import MyForm from './form/MyForm';
 
 
 function App() {
   return (
     <div className="App">
       Hello, I am Coming
+      <MyForm/>
     </div>
   );
 }
---
+++ src/form/MyForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MyForm() {
+    return (
+      <form>
+        <label>Enter your name:
+          <input type="text" />
+        </label>
+      </form>
+    )
+}
+export default MyForm;
