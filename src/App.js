6ce0f7d201ede1033d86728be3c38c519c7bb79b
Shahadat <shahadat2cp17@gmail.com>
2021-12-11 11:59:01 +0600

    ref

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import Sass from './sass/Sass';
+import Ref from './hooks/Ref/Ref';
 
 
 
-function App() {
+
+function App() {  
   return (
     <div className="App">
-      <Sass/>
+      <Ref/>
     </div>
   );
 }
---
+++ src/hooks/Ref/Ref.jsx
@@ -0,0 +1,63 @@
+import { useState, useRef } from 'react';
+import './ref.module.css'
+function Ref() {
+  const [randomInput, setRandomInput] = useState('');
+  const [seconds, setSeconds] = useState(0);
+
+  const renders = useRef(0);
+  const inputRef = useRef();
+  const timerId = useRef();
+
+  const handleChange = (e) => {
+    setRandomInput(e.target.value);
+    renders.current++;
+  }
+
+  const startTimer = () => {
+    timerId.current = setInterval(() => {
+      renders.current++;
+      setSeconds(prev => prev + 1);
+    }, 1000)
+    inputRef.current.focus();
+  }
+
+  const stopTimer = () => {
+    clearInterval(timerId.current);
+    timerId.current = 0;
+    inputRef.current.focus();
+  }
+
+  const resetTimer = () => {
+    stopTimer();
+    if (seconds) {
+      renders.current++;
+      setSeconds(0);
+    }
+    inputRef.current.focus();
+  }
+
+  return (
+    <div className="App">
+      <input
+        ref={inputRef}
+        type="text"
+        value={randomInput}
+        placeholder="Random Input"
+        onChange={handleChange}
+      />
+      <p>Renders: {renders.current}</p>
+      <br /><br />
+      <section>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
+      </section>
+      <br /><br />
+      <p>Seconds: {seconds}</p>
+      <br /><br />
+      <p>{randomInput}</p>
+    </div>
+  );
+}
+
+export default Ref;
