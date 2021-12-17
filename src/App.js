cc27b97fd914f6388492e3882e5d3569b1e1fac3
Shahadat <shahadat2cp17@gmail.com>
2021-12-17 11:07:44 +0600

    fetchapi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
-import Fetch from './hooks/effect-fetch/Fetch';
-import Fetch2 from './hooks/effect-fetch/Fetch2';
+import Recipe from './hooks/effect2/Recipe';
+// import DataFetch from './hooks/effect2/DataFetch';
+// import Fetch from './hooks/effect-fetch/Fetch';
+// import Fetch2 from './hooks/effect-fetch/Fetch2';
 
 
 
@@ -10,8 +12,10 @@ function App() {
 
   return (
     <div className="App">
-      <Fetch/>
-      <Fetch2/>
+      {/* <Fetch/>
+      <Fetch2/> */}
+      {/* <DataFetch/> */}
+      <Recipe/>
     </div>
   );
 }
---
+++ src/hooks/effect2/DataFetch.jsx
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react';
+import Recipe from './Recipe';
+
+const DataFetch = () => {
+
+
+  const APP_ID = "YOUR APP ID ";
+  const APP_KEY = "Your APP KEY";
+  const API_URL = `https://api.edamam.com/search?q=banana&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    loadData();
+  }, [recipes]);
+
+
+ const loadData = async () => {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  }
+
+  return (
+    <div className="App">
+     {
+        recipes.map((r,id) => (
+          <Recipe key={id} title={r.recipe.title} image={r.recipe.image} calories={r.recipe.calories} />
+        ))
+      }
+    </div>
+  )
+};
+
+export default DataFetch;
